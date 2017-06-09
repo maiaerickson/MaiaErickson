bae16c26ca9507d4183f4b903446fcad374ab7d6
Andrew Erickson <andrew@macroconscious.com>
2017-06-09 16:47:18 -0700

    removed comments

--- app.js
+++ app.js
@@ -28,7 +28,3 @@ app.get("/blog", function(req, res){
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-// HEy thre
-//New comment updated
-// Update 10.0
